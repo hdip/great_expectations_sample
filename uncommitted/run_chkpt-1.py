d55d46ddd3f6cd91b0fb4131c12f02d571ec87c7
hdip <developer@healthdataintegrator.com>
2021-03-19 10:22:39 -0400

    Add run id

--- uncommitted/run_chkpt-1.py
+++ uncommitted/run_chkpt-1.py
@@ -1,10 +1,12 @@
 import great_expectations as ge
 from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
 import sys
+import datetime
 
 context = ge.get_context()
+batch = 'batch-' + str(datetime.datetime.now().strftime('%Y%m%d%H%m%S'))
 data_asset_name = sys.argv[1]
-checkpoint_run_result: CheckpointResult = context.run_checkpoint(
+checkpoint_run_result: CheckpointResult = context.run_checkpoint(run_id=batch,
     checkpoint_name="chkpt-1", validations=[
         {"batch_request":
             {
